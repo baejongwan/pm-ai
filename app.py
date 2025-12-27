7e01c9c68473cf2bb03fe779f2f48cda16a80988
Jong wan Bae <bjy990711@gmail.com>
2025-12-27 15:27:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def render_top_navigation():
 # [5] 팝업창 설정
 # --------------------------------------------------------------------------
 api_key = GOOGLE_API_KEY
-selected_model = "gemini-flash-latest"
+selected_model = "gemini-1.5-flash" #gemini-1.5-flash,gemini-flash-latest
 
 if api_key:
     try:
@@ -185,3 +185,4 @@ elif target_page == "자료실": view_pdf.render_pdf_viewer("catalog.pdf")
 elif target_page == "호전반응": view_guide.render_guide(all_sheets)
 elif target_page == "체험사례": view_stories.render_experience(all_sheets)
 elif target_page == "성공사례": view_stories.render_success(all_sheets)
+
