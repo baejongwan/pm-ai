354537caeefab469a99eca5663a8cd33bed51f55
Jong wan Bae <bjy990711@gmail.com>
2026-02-04 11:43:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def render_top_navigation():
 # [5] 화면 렌더링 및 페이지 라우팅
 # --------------------------------------------------------------------------
 api_key = GOOGLE_API_KEY
-selected_model = "gemini-2.0-flash"
+selected_model = "gemini-3-flash-preview"
 
 render_home_logo()
 selected_page = render_top_navigation()
@@ -167,6 +167,7 @@ elif target_page == menu_options[9]: view_stories.render_success(all_sheets)
 elif target_page == menu_options[10]: view_videos.render_video_page(all_sheets)
 
 elif target_page == menu_options[11]: view_pdf.render_pdf_viewer("catalog.pdf")
+
 
 
 
